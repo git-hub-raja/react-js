66195cf9687a8d2362e22b9c6241f4667389c811
priyanka161 <48899131+priyanka161@users.noreply.github.com>
2019-05-25 19:37:44 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,141 @@
+import React from 'react';
+// import logo from './logo.svg';
+import './App.css';
+
+// const pstyle={
+//   top:'16px'
+// }
+function App() {
+  return (
+    <div className="App">
+          <div class="container-fluid top" >
+                  <div class="row ">
+          <div class="col-lg-2 " >
+              <div class="search">
+                <div class="form-group"> 
+                    <input class="form-control" type="text" placeholder="search" /> 
+                    <span class="fa fa-search form-control-feedback" ></span>
+                </div>
+              </div>
+          </div>
+        </div>
+        <div class="row no-gutters">
+            <div class="col-lg-2 ">
+                <div class="wid-panel">
+                  <div class="wid-sub-1"></div>
+                  <div class="wid-sub-2">course.jpg</div>
+                </div>
+            </div>
+            <div class="col-lg-2">
+                <div class="wid-panel">
+                  <div class="wid-sub-1"></div>
+                  <div class="wid-sub-2">Quiz title</div>
+                </div>
+            </div>
+            <div class="col-lg-2">
+                <div class="wid-panel">
+                  <div class="wid-sub-1"></div>
+                  <div class="wid-sub-2">course.jpg</div>
+                </div>
+            </div>
+            <div class="col-lg-2">
+                <div class="wid-panel">
+                  <div class="wid-sub-1"></div>
+                  <div class="wid-sub-2">untitled quiz</div>
+                </div>
+            </div>
+            <div class="col-lg-2">
+                <div class="wid-panel">
+                  <div class="wid-sub-1"></div>
+                  <div class="wid-sub-2">course.jpg</div>
+                </div>
+            </div>
+            <div class="col-lg-2">
+                <div class="wid-panel">
+                  <div class="wid-sub-1"></div>
+                  <div class="wid-sub-2">course.jpg</div>
+                </div>
+            </div>
+        </div>
+        <div class="row no-gutters">
+            <div class="col-lg-2">
+                <div class="wid-panel">
+                  <div class="wid-sub-1"></div>
+                  <div class="wid-sub-2">untitled quiz</div>
+                </div>
+            </div>
+            <div class="col-lg-2">
+                <div class="wid-panel">
+                  <div class="wid-sub-1"></div>
+                  <div class="wid-sub-2">course.jpg</div>
+                </div>
+            </div>
+            <div class="col-lg-2">
+                <div class="wid-panel">
+                  <div class="wid-sub-1"></div>
+                  <div class="wid-sub-2">Quiz title</div>
+                </div>
+            </div>
+            <div class="col-lg-2">
+                <div class="wid-panel">
+                  <div class="wid-sub-1"></div>
+                  <div class="wid-sub-2">Hello World!</div>
+                </div>
+            </div>
+            <div class="col-lg-2">
+                <div class="wid-panel">
+                  <div class="wid-sub-1"></div>
+                  <div class="wid-sub-2">course.jpg</div>
+                </div>
+            </div>
+            <div class="col-lg-2">
+                <div class="wid-panel">
+                  <div class="wid-sub-1"></div>
+                  <div class="wid-sub-2">Quiz title</div>
+                </div>
+            </div> 
+        </div>
+        <div class="row no-gutters">
+            <div class="col-lg-2">
+                <div class="wid-panel">
+                  <div class="wid-sub-1"></div>
+                  <div class="wid-sub-2">course.jpg</div>
+                </div>
+            </div>
+            <div class="col-lg-2">
+                <div class="wid-panel">
+                  <div class="wid-sub-1"></div>
+                  <div class="wid-sub-2">Quiz title</div>
+                </div>
+            </div>
+            <div class="col-lg-2">
+               <div class="wid-panel">
+                  <div class="wid-sub-1"></div>
+                  <div class="wid-sub-2">course.jpg</div>
+                </div>
+            </div>
+            <div class="col-lg-2">
+                <div class="wid-panel">
+                  <div class="wid-sub-1"></div>
+                  <div class="wid-sub-2">Quiz title</div>
+                </div>
+            </div>
+            <div class="col-lg-2">
+                <div class="wid-panel">
+                  <div class="wid-sub-1"></div>
+                  <div class="wid-sub-2">RecordRTC</div>
+                </div>
+            </div>
+            <div class="col-lg-2">
+                <div class="wid-panel">
+                  <div class="wid-sub-1"></div>
+                  <div class="wid-sub-2">RecordRTC</div>
+                </div>
+            </div>
+        </div>
+    </div> 
+    </div>
+  );
+}
+
+export default App;
---
+++ search.js
@@ -0,0 +1,7 @@
+import React,{component} from 'react'
+
+class Search extends component{
+    state={
+        query:
+    }   
+    
